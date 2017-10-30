e1fef3b6d7c198792228e846dd52365cb3bc4f66
Wilson, Danny <dwilson@vertafore.com>
2017-10-30 16:29:10 -0600

    added p5js and wrapper

--- src/App.js
+++ src/App.js
@@ -3,14 +3,39 @@ import logo from './taco.svg';
 import wrestler from './mexicanwrestler.svg';
 import './App.css';
 import axios from 'axios';
+import P5Wrapper from 'react-p5-wrapper';
+
 window.axios = axios;
 window.axios.defaults.baseURL = 'http://localhost:8080/';
+
+export function sketch (p) {
+    let rotation = 0;
+
+    p.setup = function () {
+        p.createCanvas(600, 400, p.WEBGL);
+    };
+
+    p.myCustomRedrawAccordingToNewPropsHandler = function (props) {
+        if (props.rotation){
+            rotation = props.rotation * Math.PI / 180;
+        }
+    };
+
+    p.draw = function () {
+        p.background(100);
+        p.noStroke();
+        p.push();
+        p.rotateY(rotation);
+        p.box(100);
+        p.ellipse(50, 50, 80, 80);
+        p.pop();
+    };
+};
 
 class App extends Component {
 
     constructor(props) {
         super(props);
-        console.log('%cHello Developer!', 'color: #ffff00 ;font-size:100px;text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;');
     };
 
     render() {
@@ -25,6 +50,7 @@ class App extends Component {
                     <p>"TO GET STARTED, EAT A TACO!"</p>
                     <button onClick={this.doStuff}>EAT</button>
                 </div>
+                <P5Wrapper sketch={sketch} ></P5Wrapper>
             </div>
         );
     };
