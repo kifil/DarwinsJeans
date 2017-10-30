4a336296e23c7c5cbb49e22506610842063eceb7
Wilson, Danny <dwilson@vertafore.com>
2017-10-30 11:43:34 -0600

    fixed cross origin request issue

--- src/main/java/io/javabrains/courseapidata/CourseApiDataApplication.java
+++ src/main/java/io/javabrains/courseapidata/CourseApiDataApplication.java
@@ -2,11 +2,26 @@ package io.javabrains.courseapidata;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class CourseApiDataApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CourseApiDataApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:3000");
+			}
+		};
 	}
 }
