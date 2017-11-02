77e6559b524c36ab5e8bd4d11d15a7d86a0d2536
Jonathan Clark <jonathan.clark@vertafore.com>
2017-11-02 15:05:04 -0600

    changed bloops to ships

--- src/bloop.js
+++ src/bloop.js
@@ -10,27 +10,6 @@
 import Creature from './creature';
 
 export default class Bloop extends Creature{
-    health = this.simulationSettings.healthLimit;
-    //constructor(p, l, dna_, type, simulationRunStats){
-       // super(p, l, dna_, type, simulationRunStats);
-   //}
 
-    // At any moment there is a teeny, tiny chance a creature will reproduce
-    reproduce() {
-        // asexual reproduction
-        //if (p.random(1) < 0.0005) {
-
-        // Creature reproduce if health is over 300, just cuz
-        if(this.health >= 300){
-            this.health -= 200;
-            // Child is exact copy of single parent
-            var childDNA = this.dna.copy();
-            // Child DNA can mutate
-            childDNA.mutate(this.simulationSettings.mutationRate);
-            return new Bloop(this.p, this.position, childDNA, this.type, this.simulationRunStats, this.simulationSettings);
-        }
-
-        return null;
-    };
 }
 
--- src/world.js
+++ src/world.js
@@ -1,11 +1,16 @@
 import Food from './food'
-import Bloop from './bloop'
+import Ship from './ship'
 import DNA from './dna'
 
 export default class World  {
     sketch = {};
     simulationRunStats = {};
     simulationSettings = {};
+    creatures = {
+        food: [],
+        ships: [],
+        kraken: [],
+    };
 
     constructor(sketch,simulationRunStats, simulationSettings){
         this.sketch = sketch;
@@ -14,20 +19,13 @@ export default class World  {
 
         // Start with initial food and creatures
         this.food = new Food(sketch, this.simulationSettings);
-        this.bloops = [];        // An array for all creatures
+        this.creatures.ships = [];
         for (var i = 0; i < this.simulationSettings.preyCount; i++) {
             var l = sketch.createVector(sketch.random(sketch.width), sketch.random(sketch.height));
             var dna = new DNA(sketch);
-            this.bloops.push(new Bloop(sketch, l, dna, 'bloop', this.simulationRunStats, this.simulationSettings));
+            this.creatures.ships.push(new Ship(sketch, l, dna, 'ship', this.simulationRunStats, this.simulationSettings));
         }
     }
-
-    // Make a new creature
-    // this.born = function(x, y) {
-    //     var l = sketch.createVector(x, y);
-    //     var dna = new DNA(sketch);
-    //     this.bloops.push(new Bloop(sketch, l, dna));
-    // }
 
     // Run the world
     run() {
@@ -35,19 +33,19 @@ export default class World  {
         this.food.run();
 
         // Cycle through the ArrayList backwards b/c we are deleting
-        for (var i = this.bloops.length-1; i >= 0; i--) {
+        for (var i = this.creatures.ships.length-1; i >= 0; i--) {
             // All bloops run and eat
-            var b = this.bloops[i];
+            var b = this.creatures.ships[i];
             b.run();
             b.eat(this.food);
             // If it's dead, kill it and make food
             if (b.dead()) {
-                this.bloops.splice(i, 1);
+                this.creatures.ships.splice(i, 1);
                 this.food.add(b.position);
             }
             // Perhaps this bloop would like to make a baby?
             var child = b.reproduce();
-            if (child != null) this.bloops.push(child);
+            if (child != null) this.creatures.ships.push(child);
         }
 
         this.simulationRunStats.worldTicks++;
