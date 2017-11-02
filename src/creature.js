e3bdc5fa8b71117a2c087b30507dc911d612d3ce
Jonathan Clark <jonathan.clark@vertafore.com>
2017-11-01 19:04:56 -0600

    Bloops are now a class that inherits from Creature and has custom health

---
+++ src/creature.js
@@ -0,0 +1,129 @@
+import p5 from 'p5';
+
+export default class Creature {
+    health = -1;
+    position = {};
+    xoff = 0;
+    yoff = 0;
+    dna = {};   // DNA
+    simulationRunStats = {};
+    currentVelocity = {};
+
+    constructor(p, l, dna_, simulationRunStats){
+        this.p = p;
+        this.l = l;
+        this.dna = dna_;
+        this.simulationRunStats = simulationRunStats;
+
+        this.position = l.copy();  // Location
+        this.xoff = p.random(1000);  // For perlin noise
+        this.yoff = p.random(1000);
+        this.simulationRunStats = simulationRunStats;
+        this.currentVelocity = {};
+
+        // DNA will determine size and maxspeed
+        // The bigger the creature, the slower it is
+        this.maxspeed = p.map(this.dna.genes[0], 0, 1, 15, 0);
+        this.r = p.map(this.dna.genes[0], 0, 1, 0, 50);
+    }
+
+    run = function() {
+        this.update();
+        this.borders();
+        this.display();
+    };
+
+    // A creature can find food and eat it
+    eat = function(f) {
+        var food = f.getFood();
+        // Are we touching any food objects?
+        for (var i = food.length-1; i >= 0; i--) {
+            var foodLocation = food[i];
+            var d = p5.Vector.dist(this.position, foodLocation);
+            // If we are, juice up our strength!
+            if (d < this.r/2) {
+                //this.foodEaten++;
+                this.simulationRunStats.foodEaten++;
+                this.health += 100;
+                food.splice(i,1);
+            }
+        }
+    };
+
+    // At any moment there is a teeny, tiny chance a creature will reproduce
+    reproduce = function() {
+        // asexual reproduction
+        //if (p.random(1) < 0.0005) {
+
+        // Creature reproduce if health is over 300, just cuz
+        if(this.health >= 300){
+            this.health -= 200;
+            // Child is exact copy of single parent
+            var childDNA = this.dna.copy();
+            // Child DNA can mutate
+            childDNA.mutate(0.01);
+            return new Creature(this.p, this.position, childDNA, this.simulationRunStats);
+        }
+
+        return null;
+    };
+
+    // Method to display
+    display = function() {
+        /*
+        p.ellipseMode(p.CENTER);
+        p.stroke(0,255);
+
+        p.ellipse(this.position.x, this.position.y, this.r, this.r);
+*/
+        this.p.rectMode(this.p.CORNER);
+        var healthBarWidth = this.r * 2;
+        var healthPercent = this.health / 200;
+
+        // Clamp the health bar to not be longer than 100%
+        if(healthPercent > 1){
+            healthPercent = 1;
+        }
+
+        this.p.noFill();
+        this.p.rect(this.position.x - this.r,this.position.y - this.r, healthBarWidth, 8);
+        this.p.fill(255, this.health, 0, 255);
+        this.p.rect(this.position.x - this.r,this.position.y - this.r, healthBarWidth * healthPercent, 8);
+
+        let image = this.currentVelocity.x > 0 ? this.p.shipRightImg : this.p.shipLeftImg;
+        this.p.imageMode(this.p.CENTER);
+        this.p.image(image, this.position.x, this.position.y, this.r, this.r);
+    };
+
+    // Method to update position
+    update = function() {
+        // Simple movement based on perlin noise
+
+        var vx = this.p.map(this.p.noise(this.xoff),0,1,-this.maxspeed,this.maxspeed);
+        var vy = this.p.map(this.p.noise(this.yoff),0,1,-this.maxspeed,this.maxspeed);
+        this.currentVelocity = this.p.createVector(vx,vy);
+        this.xoff += 0.01;
+        this.yoff += 0.01;
+
+        this.position.add(this.currentVelocity);
+        // Death always looming
+        this.health -= 0.2;
+    };
+
+    // Wraparound
+    borders = function() {
+        if (this.position.x < -this.r) this.position.x = this.p.width+this.r;
+        if (this.position.y < -this.r) this.position.y = this.p.height+this.r;
+        if (this.position.x > this.width+this.r) this.position.x = -this.p.r;
+        if (this.position.y > this.height+this.r) this.position.y = -this.p.r;
+    };
+
+    // Death
+    dead = function() {
+        if (this.health < 0.0) {
+            this.simulationRunStats.shipDeaths++;
+            return true;
+        }
+        return false;
+    };
+}
---
+++ src/ship.js
@@ -0,0 +1,6 @@
+import p5 from 'p5';
+import Ship from './creature';
+
+export default class Ship extends Creature{
+    health = 200;
+}
