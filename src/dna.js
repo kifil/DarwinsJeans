80c3adccccf892ea41e2a347b4991c2d088e0eba
Wilson, Danny <dwilson@vertafore.com>
2017-11-02 16:38:52 -0600

    added named genes

--- src/dna.js
+++ src/dna.js
@@ -4,35 +4,38 @@
 // Class to describe DNA
 // Has more features for two parent mating (not used in this example)
 
-
 // Constructor (makes a random DNA)
-export default function DNA(p, newgenes) {
-    if (newgenes) {
-        this.genes = newgenes;
+export default function DNA(p, newGenes) {
+    this.genesList = ["Speed-Size","Carrots"];
+
+    if (newGenes) {
+        this.genes = newGenes;
     } else {
         // The genetic sequence
         // DNA is random floating point values between 0 and 1 (!!)
-        this.genes = new Array(1);
-        for (var i = 0; i < this.genes.length; i++) {
-            this.genes[i] = p.random(0,1);
+        this.genes = {};
+        for (var i = 0; i < this.genesList.length; i++) {
+            var geneName = this.genesList[i];
+            this.genes[geneName] =  p.random(0,1);
         }
     }
 
     this.copy = function() {
         // should switch to fancy JS array copy
-        var newgenes = [];
-        for (var i = 0; i < this.genes.length; i++) {
-            newgenes[i] = this.genes[i];
+        var newGenes = {};
+        for (var key in this.genes) {
+            newGenes[key] = this.genes[key];
         }
 
-        return new DNA(p, newgenes);
+        return new DNA(p, newGenes);
     }
 
     // Based on a mutation probability, picks a new random character in array spots
     this.mutate = function(mutationRate) {
-        for (var i = 0; i < this.genes.length; i++) {
+        for (var key in this.genes) {
             if (p.random(1) < mutationRate) {
-                this.genes[i] = p.random(0,1);
+                console.log("I mutated", mutationRate);
+                this.genes[key] = p.random(0,1);
             }
         }
     }
---
+++ src/genome.js
@@ -0,0 +1,8 @@
+export default class Genome {
+    genesList = ["Speed-Size","Carrots"];
+
+    constructor(geneName,geneValue){
+        this.geneName = geneName;
+        this.geneValue = geneValue;
+    }
+}
